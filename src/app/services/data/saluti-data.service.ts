0f4c327aacbcbf34cb452489e30b3935ac20d8a9
Simone Serra <sserra@outlook.it>
2021-02-15 17:18:28 +0100

    lezione 54 (Docker)

---
+++ src/app/services/data/saluti-data.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthappService } from '../authapp.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalutiDataService {
+
+  constructor(private httpClient : HttpClient, private BasicAuth: AuthappService) { }
+
+  getSaluti() {
+    let nome = this.BasicAuth.loggedUser();
+    return this.httpClient.get("http://localhost:5000/api/saluti/"+"Marco");
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthappService } from '../services/authapp.service';
+import { SalutiDataService } from '../services/data/saluti-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,16 +10,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
-  // messaggio = 'Saluti sono il componente welcome'
+  messaggio = ''
   saluti: string = 'Benvenuti nel sito Alphashop'
   titolo2 = "Seleziona gli articoli da acquistare"
   utente = ''
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private salutiSrv : SalutiDataService) { }
 
   ngOnInit(): void {
     this.utente = this.route.snapshot.params['userid']
     // console.log(this.messaggio);
   }
 
+  getSaluti() {
+    this.salutiSrv.getSaluti().subscribe(
+      response => this.handleResponse(response),
+      error => this.handleError(error)
+    )
+  }
+
+  handleResponse(response : Object) {
+    this.messaggio = response.toString();
+  }
+
+  handleError(error : Object) {
+    //this.messaggio = error.toString();
+  }
 }
